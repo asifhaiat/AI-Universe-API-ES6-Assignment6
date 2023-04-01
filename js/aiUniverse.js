7444618d8c2411acc2b7ab1e4cd48a148d67f5db
asifhaiat <asifhaiat674652gmail.com>
2023-04-01 16:25:28 +0600

    show ai hub images and features item

--- js/aiUniverse.js
+++ js/aiUniverse.js
@@ -1,8 +1,8 @@
-// console.log('JavaScript file added');
-
 /* Load Ai Universe Hub data */
 const getAiUniverseHubData = async () => {
   try {
+    // show loading spinner
+    toggleSpinner(true);
     const url = "https://openapi.programming-hero.com/api/ai/tools";
     const res = await fetch(url);
     const data = await res.json();
@@ -15,24 +15,59 @@ const getAiUniverseHubData = async () => {
 /* Display AI Universe Hub Data in Frontend */
 const showAiTools = (tools) => {
   console.log(tools);
+  // display only 6 ai tools
+  /* const showMore = document.getElementById("show-more");
+  if (tools.length > 6) {
+    tools = tools.slice(0, 6);
+    showMore.classList.remove("hidden");
+  } else {
+    showMore.classList.add("hidden");
+  } */
+
   const aiToolsContainer = document.getElementById("ai-tools-container");
   tools.forEach((tool) => {
     const aiDiv = document.createElement("div");
+    const features = tool.features.join("</li><li>");
     aiDiv.innerHTML = `
       <div class="border border-gray-300 rounded-md p-5 mt-10 shadow-md hover:-translate-y-1 hover:ease-in duration-300">
         <img src="${tool.image}" class="w-96 rounded-md" />
         <div>
-            <h1 class="font-bold mt-5">Features</h1>
+            <h2 class="font-bold mt-2 mb-3">Features:</h2>
             <ul class="p-5 list-decimal">
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>${features}</li>
             </ul>
         </div>
       </div>
     `;
     aiToolsContainer.appendChild(aiDiv);
   });
+
+  // stop loader or spinner
+  toggleSpinner(false);
 };
 
+/* Loading Spinner Function */
+const toggleSpinner = (isLoading) => {
+  const loadingSection = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSection.classList.remove("hidden");
+  } else {
+    loadingSection.classList.add("hidden");
+  }
+};
+
+/*  add show more button event listener, add spinner,  displayed rest ofo the data*/
+document.getElementById("show-more-btn").addEventListener("click", function () {
+  // console.log("Show more button clicked");
+  // start loader
+  toggleSpinner(true);
+  // get all ai tools
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((response) => response.json())
+    .then((data) => {
+      showAiTools(data.data.tools);
+    });
+});
+
+// load the initial ai tools
 getAiUniverseHubData();
