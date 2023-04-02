4329a1efdbaee4c74e0e5a0a801a9635a295546e
asifhaiat <asifhaiat674652gmail.com>
2023-04-02 06:34:33 +0600

    add show more button and displayed all Ai hub and also hide show more button

--- js/aiUniverse.js
+++ js/aiUniverse.js
@@ -1,35 +1,34 @@
 /* Load Ai Universe Hub data */
+let toolsData = [];
+
 const getAiUniverseHubData = async () => {
   try {
     // show loading spinner
     toggleSpinner(true);
 
-    // wait for 2 seconds to simulate loading time
+    // wait for 1 second to simulate loading time
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const url = "https://openapi.programming-hero.com/api/ai/tools";
     const res = await fetch(url);
     const data = await res.json();
-    return showAiTools(data.data.tools);
+    toolsData = data.data.tools;
+    showAiTools(toolsData.slice(0, 6));
   } catch (error) {
     console.error(`Error loading AI Universe Hub data: ${error}`);
   }
 };
 
 /* Display AI Universe Hub Data in Frontend */
-const showAiTools = (tools) => {
-  // console.log(tools);
 
-  // display only 6 ai tools
-  const showMore = document.getElementById("show-more");
-  if (tools.length > 6) {
-    tools = tools.slice(0, 6);
-    showMore.classList.remove("hidden");
-  } else {
-    showMore.classList.add("hidden");
-  }
+const showAiTools = (tools) => {
+  console.log(tools);
+
+  // hide the spinner
+  toggleSpinner(false);
 
   const aiToolsContainer = document.getElementById("ai-tools-container");
+
   tools.forEach((tool) => {
     const aiDiv = document.createElement("div");
     aiDiv.innerHTML = `
@@ -62,8 +61,18 @@ const showAiTools = (tools) => {
     aiToolsContainer.appendChild(aiDiv);
   });
 
-  // stop loader or spinner
-  toggleSpinner(false);
+  // initially it shows first six tools
+  const showMore = document.getElementById("show-more");
+  if (toolsData.length > tools.length + document.querySelectorAll(".show-more").length) {
+    showMore.classList.remove("hidden");
+  } else {
+    showMore.classList.add("hidden");
+  }
+
+  // hide "Show more" button if all tools are displayed
+  if (toolsData.length === document.querySelectorAll(".border").length) {
+    document.getElementById("show-more").classList.add("hidden");
+  }
 };
 
 /* Loading Spinner Function */
@@ -76,15 +85,25 @@ const toggleSpinner = (isLoading) => {
   }
 };
 
-/*  add show more button event listener, add spinner,  displayed rest ofo the data*/
-document.getElementById("show-more-btn").addEventListener("click", function () {
-  // console.log("Show more button clicked");
-
-  // start loader
+document.getElementById("show-more-btn").addEventListener("click", async function () {
+  // show loading spinner
   toggleSpinner(true);
 
-  // load more data
-  getAiUniverseHubData();
+  try {
+    const currentToolsLength = document.querySelectorAll(".border").length;
+    const nextTools = toolsData.slice(currentToolsLength, currentToolsLength + 6);
+
+    // wait for 1 second to simulate loading time
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // display more tools
+    showAiTools(nextTools);
+  } catch (error) {
+    console.error(`Error loading more AI tools: ${error}`);
+  } finally {
+    // hide loading spinner
+    toggleSpinner(false);
+  }
 });
 
 // load the initial ai tools
